6f3ab74fdc40aa99ba227fc72d7de7a826d9921b
hoparkgo9ma <kpjhg0124@naver.com>
2018-01-26 22:16:27 +0900

    이제 문장속에 들어간 짭그라던가.. 물음표에도 대답함
    ???: 이 코드는 정말 무식한것 같아 짭그야 그렇지?
    짭그: 17

--- bot.py
+++ bot.py
@@ -5,21 +5,40 @@ import datetime
 import time
 
 bot_token = ''
+channel_id = ''
 zabgrassCall = '짭그'
 
 client = discord.Client()
 
-auto = ['퇴근 언제하지 ㅠ', '흠', '여튼 카처가 나빴어요.']
+null = ['', '', '', '']
+
+auto = [
+    '퇴근 언제하지 ㅠ', 
+    '흠', 
+    '여튼 카처가 나빴어요.'
+    ]
 day = []
-night = ['야근이 싫어요', '오늘도 밤샘이다!']
-reply = ['', '네? 왜요?', '부르지 마세요', '아니에요']
+night = [
+    '야근이 싫어요', 
+    '오늘도 밤샘이다!'
+    ]
+reply = [
+    '네? 왜요?', 
+    '부르지 마세요', 
+    '아니에요'
+    ]
+
+ask = [
+    '?',
+    '어떰'
+    ]
 
 async def autoSendMessage():
     await client.wait_until_ready()
     ContentList = work()
     while not client.is_closed:
         Content = random.choice(ContentList)
-        await client.send_message(client.get_channel(''), Content)
+        await client.send_message(client.get_channel(channel_id), Content)
         wait = waittime()
         print('다음 메시지는 '+str(wait)+'초 후에.')
         await asyncio.sleep(wait)
@@ -34,13 +53,23 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    done = False
     msg = message.content
-    if zabgrassCall in msg:
-        Content = random.choice(reply)
-        try:
-            await client.send_message(client.get_channel(''), Content)
-        except:
-            pass
+    Content = random.choice(reply)
+    if client.user.id != message.author.id:
+        for i in range(len(ask)):
+            print(msg.find(ask[i]))
+            if msg.find(ask[i]) >= 0 and done == False:
+                await client.send_message(client.get_channel(channel_id), Content)
+                done = True
+                break
+        if msg.find('짭그') >= 0 and done == False:
+            try:
+                await client.send_message(client.get_channel(channel_id), Content)
+                done = True
+            except:
+                done = True
+    
 
 def Datetime():
     utcnow = datetime.datetime.utcnow()
@@ -50,9 +79,9 @@ def Datetime():
     return KRtime
 
 def waittime():
-    hour = [2, 3, 4, 5, 6, 7]
-    min_ten = [1, 2, 3, 4, 5]
-    min_one = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    hour = list(range(2, 8))
+    min_ten = list(range(1, 6))
+    min_one = list(range(0, 10))
     time = random.choice(hour)*60*60 + random.choice(min_ten)*60*10 + random.choice(min_one)*60
     return time
 
